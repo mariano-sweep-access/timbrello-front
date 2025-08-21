a7566403bc0b5b4763a39c54370047993a152b64
mariano sabatucci <marosaba83@gmail.com>
2025-08-20 23:14:28 -0300

    Add custom fonts and update Tailwind configuration with new color and font settings

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+      <Text className="text-5xl text-center font-bold font-quicksand-bold text-primary">
         Welcome to Nativewind!
       </Text>
     </View>
